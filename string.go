bd7b604cdf519c1c7a624c0c751c5016a515a390
kadekframa <dframa19@gmail.com>
2022-10-25 18:16:44 +0800

    First Commit. Learning by doing Golang. Number, string, boolean. Tetap Semangat!

---
+++ string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Kadek")
+	fmt.Println("Kadek Frama")
+	fmt.Println("Kadek Frama Danamastyana")
+
+	fmt.Println(len("Kadek"))
+	fmt.Println("Kadek Frama"[0])
+	fmt.Println("Kadek Frama Danamastyana"[1])
+}
